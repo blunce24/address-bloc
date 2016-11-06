ad6d5202f8a54588c4ff51af953024c4dca3ca3e
blunce24 <blairlunceford@gmail.com>
2016-11-06 10:19:11 -0600

    Assignment Complete Search

--- models/address_book.rb
+++ models/address_book.rb
@@ -44,7 +44,17 @@ class AddressBook
         lower = mid + 1
       end
     end
-    
+
+    return nil
+  end
+
+  def iterative_search(name)
+    entries.each do |entry|
+      if name == entry.name
+        return entry
+      end
+    end
+
     return nil
   end
 end
