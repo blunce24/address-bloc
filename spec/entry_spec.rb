66a6ae36f93c37210a12256fed7d3d37c3579b3d
blunce24 <blairlunceford@gmail.com>
2016-11-06 09:27:42 -0600

    Completed checkpoint CSV

--- spec/entry_spec.rb
+++ spec/entry_spec.rb
@@ -1,8 +1,8 @@
 require_relative '../models/entry'
 
 RSpec.describe Entry do
+  let(:entry) { Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com') }
   describe "attributes" do
-    let(:entry) { Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com') }
 
     it "responds to name" do
       expect(entry).to respond_to(:name)
@@ -31,7 +31,6 @@ RSpec.describe Entry do
 
   describe "#to_s" do
     it "prints an entry as a string" do
-      entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
       expected_string = "Name: Ada Lovelace\nPhone Number: 010.012.1815\nEmail: augusta.king@lovelace.com"
       expect(entry.to_s).to eq(expected_string)
     end
